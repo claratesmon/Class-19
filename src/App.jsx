b4bd9ff30a86f0961fd3c591ab2316be296d6f79
marshall-bits <kz_@hotmail.es>
2023-11-21 10:49:56 +0100

    solution exercise

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,15 @@
 /* eslint-disable react/prop-types */
 import './App.css'
-import { useState } from 'react'; // Usestate is a react HOOK
+import Card from './components/Card'
 
 
 const App = () => { // This is also a functional component
-  const [count, setCount] = useState(0); // this is how we define a state in react
-  console.log("This component is mounted");
+
 
 
   return (
     <>
-      <h1>counter: {count}</h1>
-      <button onClick={() => setCount(count + 1)}>+</button>
-      <button onClick={() => setCount(count - 1)}>-</button>
+      <Card />
     </>
   )
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,16 +1,20 @@
 /* eslint-disable react/prop-types */
 import './card.css'
+import { useState } from 'react';
 
-function Card({username, age, imageurl}){
-    //const {username, age, imageurl} = props;
-    console.log(username);
+function Card(){
+    const [username, setUsername] = useState("Michael Scott");
+  
     return(
         <div className="card-container">
-            <h3>{username}</h3>
-            <p>{age} years old</p>
-            <img src={imageurl} alt="avatar" />
+            <h1>{username}</h1>
+            <p>This is the world{"'"}s best boss</p>
+            <img className='avatar' src="https://es.rollingstone.com/wp-content/uploads/2021/12/Los-mejores-personajes-de-Michael-Scott.jpg" alt="avatar" />
+            <button onClick={()=> setUsername("Prision Mike")}>Change the name</button>
         </div>
     )
 }
 
 export default Card;
+
+// https://i.blogs.es/662d1e/dwight-office/1366_2000.jpeg
