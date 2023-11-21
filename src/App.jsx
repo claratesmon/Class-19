b831481700a5ec52db46b98e2e05b0d529337e82
marshall-bits <kz_@hotmail.es>
2023-11-21 12:24:59 +0100

    add movies

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,14 @@
 /* eslint-disable react/prop-types */
 import './App.css'
-import Card from './components/Card'
+import MovieList2 from './components/MoviesList2';
 
 
 const App = () => { // This is also a functional component
   console.log("APP is mounted");
-  const arrayOfLi = ["Rick", "Morty", "Sam"]
 
   return (
     <>
-      <ul>
-        {arrayOfLi.map((dittoName, dittoIndex) => {
-          return (
-            <li key={dittoIndex}>
-              <h1>{dittoName}</h1>
-              <p >{dittoName}</p>
-            </li>
-          )
-        })}
-      </ul>
-      {/* <Card /> */}
+      <MovieList2 />
     </>
   )
 }
